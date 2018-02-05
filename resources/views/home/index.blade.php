83afc37936f94ed514d67a7184b30466d111ebff
麦奇 <feng@fennay.com>
2018-02-05 17:41:20 +0800

    标签空格调整

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -15,8 +15,8 @@
         @if(!empty($articleList))
             @foreach($articleList as $k=> $vo)
                 <div class="article">
-                    <h3>{{substr($vo->created_at,0,10)}} [ &nbsp;@foreach($vo->tags as $item)<a
-                    href="{{route('tag',['tags' => $item->name])}}">{{$item->name}}</a> &nbsp;@endforeach] <a href="{{route('articleDetail',['url'=>$vo->url])}}">{{$vo->title}}</a></h3>
+                    <h3>{{substr($vo->created_at,0,10)}} [ @foreach($vo->tags as $item)<a
+                    href="{{route('tag',['tags' => $item->name])}}">{{$item->name}}</a> @endforeach] <a href="{{route('articleDetail',['url'=>$vo->url])}}">{{$vo->title}}</a></h3>
                 </div>
             @endforeach
         @endif
